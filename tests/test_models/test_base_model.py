33fecbb3e92278bf862f204a0508fa9e99b8af46
rolandoquiroz <rolandoquiroz@gmail.com>
2023-07-13 06:25:58 -0500

    Add tests files for  Store first object

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,27 +17,27 @@ class TestBaseModel(unittest.TestCase):
     A TestBaseModel Class
     """
     def setUp(self):
-        self.mdl = BaseModel()
+        self.bmdl = BaseModel()
         self.start = time()
 
     def tearDown(self):
         self.end = time()
 
     def test_id(self):
-        self.assertTrue(self.mdl.id)
+        self.assertTrue(self.bmdl.id)
 
     def test_created_at(self):
-        self.assertIs(type(self.mdl.created_at), datetime)
+        self.assertIs(type(self.bmdl.created_at), datetime)
 
     def test_str(self):
-        self.assertEqual(str(self.mdl),
-                         f'[BaseModel] ({self.mdl.id}) {self.mdl.__dict__}')
+        self.assertEqual(str(self.bmdl),
+                         f'[BaseModel] ({self.bmdl.id}) {self.bmdl.__dict__}')
 
     def test_save(self):
-        first_time = self.mdl.updated_at
-        self.mdl.save()
-        second_time = self.mdl.updated_at
+        first_time = self.bmdl.updated_at
+        self.bmdl.save()
+        second_time = self.bmdl.updated_at
         self.assertNotEqual(first_time, second_time)
 
     def test_to_dict(self):
-        self.assertIn('__class__', self.mdl.to_dict())
+        self.assertIn('__class__', self.bmdl.to_dict())
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,1 +1,62 @@
 #!/usr/bin/python3
+
+"""
+
+This module define a TestFileStorage Class
+
+"""
+
+import unittest
+from time import time
+import json
+import os
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+
+
+class TestFileStorage(unittest.TestCase):
+    """
+    A TestFileStorage Class
+    """
+
+    def setUp(self):
+        self.strg = FileStorage()
+        self.bmdl = BaseModel()
+        self.start = time()
+
+    def tearDown(self):
+        self.end = time()
+    
+    def test_file_path(self): 
+        self.assertEqual(self.strg._FileStorage__file_path, 'file.json')
+
+    def test_objects(self):
+        self.assertEqual(self.strg._FileStorage__objects, {})
+
+    def test_all(self):
+        self.assertEqual(self.strg.all(), {})
+
+    def test_new(self):
+        self.strg.new(self.bmdl)
+        self.assertDictEqual(self.strg.all(), 
+                             {f'BaseModel.{self.bmdl.id}': self.bmdl})
+        self.strg.all().clear()
+    
+    def test_save(self):
+        self.strg.new(self.bmdl)
+        self.strg.save()
+
+        with open('file.json', 'r') as f:
+            json_obj = json.loads(f.read())
+
+        self.assertDictEqual(json_obj,
+                             {f'BaseModel.{self.bmdl.id}': self.bmdl.to_dict()})
+
+    def test_reload(self):
+        self.strg.__objects = {}
+        self.assertEqual(len(self.strg.all()), 0)
+        self.bmdl.save()
+        self.strg.__objects = {}
+        self.strg.reload()
+        self.assertNotEqual(len(self.strg.all()), 0)
